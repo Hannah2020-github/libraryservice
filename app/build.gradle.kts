d6c8313e666f12d64c21815889d4ede52fb61b22
Hannah2020-github <devonclass712@gmail.com>
2024-08-23 14:48:11 +0800

    feature: gradle import room API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ android {
     }
 }
 
+
 dependencies {
 
     implementation(libs.androidx.core.ktx)
@@ -66,4 +67,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
 }
