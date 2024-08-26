d2d3008bf6f51feecc8d86b6a6863680625b25db
Hannah2020-github <devonclass712@gmail.com>
2024-08-26 14:12:39 +0800

    feature: 測試 Database 是否能順利執行

--- app/src/main/java/com/hannah/libraryservice/activities/MainActivity.kt
+++ app/src/main/java/com/hannah/libraryservice/activities/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.hannah.libraryservice.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import com.hannah.libraryservice.R
+import com.hannah.libraryservice.db.AppDatabase
+import com.hannah.libraryservice.db.User
+import com.hannah.libraryservice.db.UserDao
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        val appDatabase = AppDatabase.buildDatabase(this)
+        val userDao = appDatabase.getUserDao()
+        val bookDao = appDatabase.getBookDao()
+
+        Thread {
+            val user1 = User("1", "Mary", "book1", "book2", "book3", "book4")
+            userDao.insertOneNewUser(user1)
+            Log.d("CCC", "${userDao.getAllUsers()}")
+        }.start()
+
 
 
     }
