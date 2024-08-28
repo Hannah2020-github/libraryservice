0a35a607945a6a9d36ea5fdf9bf2d5a607e5f034
Hannah2020-github <devonclass712@gmail.com>
2024-08-28 16:38:51 +0800

    feature: 儲存 book 資料

--- app/src/main/java/com/hannah/libraryservice/activities/BookRegisterActivity.kt
+++ app/src/main/java/com/hannah/libraryservice/activities/BookRegisterActivity.kt
@@ -2,18 +2,23 @@ package com.hannah.libraryservice.activities
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.hannah.libraryservice.R
+import com.hannah.libraryservice.db.AppDatabase
+import com.hannah.libraryservice.db.Book
+import java.util.concurrent.Executors
 
 class BookRegisterActivity : AppCompatActivity() {
     private lateinit var bookName: EditText
     private lateinit var bookISBN: EditText
     private lateinit var bookRegisterBtn: Button
     private lateinit var bookRegisterResult: TextView
+    private val sigleThreadException =  Executors.newSingleThreadExecutor()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,6 +71,24 @@ class BookRegisterActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
             // 以上條件滿足，則儲存 book 資料
+            sigleThreadException.execute {
+                try {
+                    val bookTitle = bookName.text.toString()
+                    val bookISBNEntry = bookISBN.text.toString()
+                    var db = AppDatabase.buildDatabase(this)
+                    db.getBookDao().insertOneNewBook(Book(bookISBNEntry, bookTitle, null))
+                    runOnUiThread {
+                        bookRegisterResult.text = "The result is:\nCongrats! The book has been registered."
+                        bookName.setText("")
+                        bookISBN.setText("")
+                    }
+                }catch (e: Exception) {
+                    runOnUiThread {
+                        bookRegisterResult.text = "The result is :\n${e.message}"
+                    }
+
+                }
+            }
 
         }
     }
@@ -75,7 +98,6 @@ class BookRegisterActivity : AppCompatActivity() {
         // 找出前 9 項
         // 前 9 項的加權和，再去跟最後一項去 mod
         for (i in 0 until isbn.length - 1) {
-            // 此數字為 char，利用 - '0'，轉換成 Int 數字
             sum += (isbn[i] - '0') * (i + 1)
         }
 
