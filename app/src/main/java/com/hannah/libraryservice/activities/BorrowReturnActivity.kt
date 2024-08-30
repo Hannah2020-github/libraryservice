1002c80adae23c6bd776bc9e279229ac7390c135
Hannah2020-github <devonclass712@gmail.com>
2024-08-30 17:12:33 +0800

    feature: 借書功能(borrow)

--- app/src/main/java/com/hannah/libraryservice/activities/BorrowReturnActivity.kt
+++ app/src/main/java/com/hannah/libraryservice/activities/BorrowReturnActivity.kt
@@ -2,15 +2,20 @@ package com.hannah.libraryservice.activities
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import com.hannah.libraryservice.R
+import com.hannah.libraryservice.db.AppDatabase
+import com.hannah.libraryservice.db.Book
+import com.hannah.libraryservice.db.User
 import com.hannah.libraryservice.util.IdUtil
 import com.hannah.libraryservice.util.IdUtil.Companion.checkUserId
 import com.hannah.libraryservice.util.IsbnUtil.Companion.checkIsbn
+import java.util.concurrent.Executors
 
 class BorrowReturnActivity : AppCompatActivity() {
     private lateinit var bookISBN: EditText
@@ -18,6 +23,8 @@ class BorrowReturnActivity : AppCompatActivity() {
     private lateinit var borrowBtn: Button
     private lateinit var returnBtn: Button
     private lateinit var result: TextView
+
+    private val singleThreadExecutor =  Executors.newSingleThreadExecutor()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,10 +43,107 @@ class BorrowReturnActivity : AppCompatActivity() {
             if (!checkIsbn(bookIsbn, this)) {
                 return@setOnClickListener
             }
-
             if (!checkUserId(userId, this)) {
                 return@setOnClickListener
             }
+            // 借書
+            singleThreadExecutor.execute {
+                try {
+                    val db = AppDatabase.buildDatabase(this)
+                    val userDao = db.getUserDao()
+                    val user = userDao.getUserById(userId.uppercase())
+                    if (user == null) {
+                        runOnUiThread {
+                            result.text = "User not found. Borrow failed."
+                        }
+                        return@execute
+                    }
+
+                    val bookDao = db.getBookDao()
+                    val book = bookDao.getBookByISBN(bookIsbn)
+                    if (book == null) {
+                        runOnUiThread {
+                            result.text = "Book not found. Borrow failed."
+                        }
+                        return@execute
+                    }
+
+                    if (user.Borrowing1 != null &&
+                        user.Borrowing2 != null &&
+                        user.Borrowing3 != null &&
+                        user.Borrowing4 != null ) {
+                        runOnUiThread {
+                            result.text = "User has reached the book borrowing limit. Borrow failed."
+                        }
+                        return@execute
+                    }
+
+                    if (book.borrowTo != null) {
+                        runOnUiThread {
+                            result.text = "This book is borrowed to someone else already. Borrow failed."
+                        }
+                        return@execute
+                    }
+
+                    if (user.Borrowing1 == null) {
+                        userDao.updateOneUser(
+                            User(
+                                user.id,
+                                user.fullName,
+                                bookIsbn,
+                                user.Borrowing2,
+                                user.Borrowing3,
+                                user.Borrowing4
+                            )
+                        )
+                    }else if (user.Borrowing2 == null) {
+                        userDao.updateOneUser(
+                            User(
+                                user.id,
+                                user.fullName,
+                                user.Borrowing1,
+                                bookIsbn,
+                                user.Borrowing3,
+                                user.Borrowing4
+                            )
+                        )
+                    }else if (user.Borrowing3 == null) {
+                        userDao.updateOneUser(
+                            User(
+                                user.id,
+                                user.fullName,
+                                user.Borrowing1,
+                                user.Borrowing2,
+                                bookIsbn,
+                                user.Borrowing4
+                            )
+                        )
+                    } else if (user.Borrowing4 == null) {
+                        userDao.updateOneUser(
+                            User(
+                                user.id,
+                                user.fullName,
+                                user.Borrowing1,
+                                user.Borrowing2,
+                                user.Borrowing3,
+                                bookIsbn
+                            )
+                        )
+                    }
+                    bookDao.updateOneBook(Book(book.isbn, book.bookName, user.id))
+                    runOnUiThread {
+                        result.text = "Borrow success!"
+                        bookISBN.setText("")
+                        userID.setText("")
+                    }
+                }catch (e: Exception) {
+                    result.text = e.message
+                    e.printStackTrace()
+                    Log.d("AAA", "not find user...123")
+                }
+            }
+
+
         }
 
         returnBtn.setOnClickListener {
