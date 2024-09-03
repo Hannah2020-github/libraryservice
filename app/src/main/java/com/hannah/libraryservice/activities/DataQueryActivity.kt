d60261335bff4b88183756320258cd1646dc2d87
Hannah2020-github <devonclass712@gmail.com>
2024-09-03 16:28:40 +0800

    feature: 輸入 data id，查詢相關資訊

--- app/src/main/java/com/hannah/libraryservice/activities/DataQueryActivity.kt
+++ app/src/main/java/com/hannah/libraryservice/activities/DataQueryActivity.kt
@@ -2,14 +2,18 @@ package com.hannah.libraryservice.activities
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.hannah.libraryservice.R
 import com.hannah.libraryservice.db.AppDatabase
 import com.hannah.libraryservice.db.Book
+import com.hannah.libraryservice.util.IdUtil
+import com.hannah.libraryservice.util.IsbnUtil
 import java.util.concurrent.Executors
 
 class DataQueryActivity : AppCompatActivity() {
@@ -30,6 +34,75 @@ class DataQueryActivity : AppCompatActivity() {
         AllUsersBtn = findViewById(R.id.find_all_user_btn)
         AllBooksBtn = findViewById(R.id.find_all_book_btn)
         result = findViewById(R.id.data_query_result)
+
+        searchBtn.setOnClickListener {
+            val id = dataId.text.toString()
+            if (id == "") {
+                val ab = AlertDialog.Builder(this)
+                ab.setTitle("ID Error")
+                ab.setMessage("ID cannot is be empty.")
+                ab.setCancelable(false)
+                ab.setPositiveButton("Okay") {dialog, _ -> dialog.dismiss()}
+                ab.create().show()
+                return@setOnClickListener
+            }
+            // 首字如果是英文字，則為身分證號碼
+            if (id[0].isLetter() && !IdUtil.checkUserId(id, this)) {
+                return@setOnClickListener
+            // 首字如果不是英文字，則為 ISBN 書碼
+            }else if (!id[0].isLetter() && !IsbnUtil.checkIsbn(id, this)) {
+                return@setOnClickListener
+            }
+
+            singleThreadExecutor.execute {
+                try {
+                    var resultText = ""
+                    // 使用身分證號碼查詢
+                    if (id[0].isLetter()) {
+                        id[0].uppercase()
+                        val userDao = AppDatabase.buildDatabase(this).getUserDao()
+                        val bookDao = AppDatabase.buildDatabase(this).getBookDao()
+                        val user = userDao.getUserById(id.uppercase())
+                        if (user != null) {
+                            resultText += "User name is ${user.fullName}\n"
+                            resultText += "User ID is ${user.id}\n"
+                            resultText += "The books this user is borrowing: \n"
+
+                            var isBorrowingBooks = false
+                            val bookBorrowing = arrayOf(user.Borrowing1, user.Borrowing2, user.Borrowing3, user.Borrowing4)
+                            for (i in bookBorrowing.indices) {
+                                if (bookBorrowing[i] != null) {
+                                    isBorrowingBooks = true
+                                    val bookFound = bookDao.getBookByISBN(bookBorrowing[i]!!)
+                                    resultText += "${bookFound.isbn} ${bookFound.bookName}\n"
+                                }
+                            }
+                            if (!isBorrowingBooks) {
+                                resultText += "none\n"
+                            }
+
+                        }else {
+                            resultText += "User not found."
+                        }
+                    // 使用 ISBN 書碼查詢
+                    } else {
+                        val bookDao = AppDatabase.buildDatabase(this).getBookDao()
+                        val book = bookDao.getBookByISBN(id)
+                        resultText += if (book != null) {
+                            "${book.isbn} : ${book.bookName}"
+                        }else {
+                            "Book not found."
+                        }
+                    }
+
+                    runOnUiThread {
+                        result.text = resultText
+                    }
+                }catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
 
         AllUsersBtn.setOnClickListener {
             singleThreadExecutor.execute {
